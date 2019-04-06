c59ecfa3c77a62b53edaa2403316e809fa35d954
Lydia Hallie <lydiajuliettehallie@gmail.com>
2019-04-06 22:34:21 +0200

    Generate simulated sensor data

---
+++ src/distributed/sensors/sensor.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+var name = flag.String("name", "sensor", "name of the sensor")
+var freq = flag.Uint("freq", 5, "update frequency in cycles/sec")
+var max = flag.Float64("max", 5., "maximum value for generated readings")
+var min = flag.Float64("min", 1., "minimum value for generated readings")
+var stepSize = flag.Float64("step", 0.1, "maximum allowable change per measurement")
+
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+var value = r.Float64()*(*max-*min) + *min
+var nom = (*max-*min)/2 + *min
+
+func main() {
+	flag.Parse()
+
+	dur, _ := time.ParseDuration(strconv.Itoa(1000/int(*freq)) + "ms")
+
+	signal := time.Tick(dur)
+
+	for range signal {
+		calcValue()
+		log.Printf("Reading sent. Value: %v\n", value)
+	}
+}
+
+func calcValue() {
+	var maxStep, minStep float64
+
+	if value < nom {
+		maxStep = *stepSize
+		minStep = -1 * *stepSize * (value - *min) / (nom - *min)
+	} else {
+		maxStep = *stepSize * (*max - value) / (*max - nom)
+		minStep = -1 * *stepSize
+	}
+
+	value += r.Float64()*(maxStep-minStep) + minStep
+}
